0495140a51d34a9ed6b34b0a0000ab2a9d2b6adc
ColtonLeese <coltleese@gmail.com>
2017-12-05 01:27:47 -0500

    adding testing file and first search_east test

---
+++ word_search_kata_specs.py
@@ -0,0 +1,20 @@
+import word_search_kata
+import unittest
+
+#word_search_kata.main('word-search-content.txt')
+
+#simulating the letter grid
+letter_grid_3x3 = [['t','h','e'], ['o','o','a'], ['o','w','t']]
+
+class wordSearchTDD(unittest.TestCase):
+
+	def test_search_east(self):
+		x = 0
+		y = 0 
+		letter_index = 0
+		word = 'the' 
+		coordinates = word_search_kata.search_east(x,y,letter_index,word,letter_grid_3x3)
+		self.assertEqual( [(0,0),(0,1),(0,2)] , coordinates)
+
+if __name__ == '__main__':
+	unittest.main()		
