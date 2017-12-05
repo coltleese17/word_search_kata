45aea62b840400680bf33f9db57119a0823e1135
ColtonLeese <coltleese@gmail.com>
2017-12-05 00:56:26 -0500

    first commit

---
+++ word_search_kata.py
@@ -0,0 +1,19 @@
+import sys
+import pandas as pd
+
+def main(text_file):
+	with open(text_file, "r") as tf:
+
+		#pull in a list of words from the first line
+		word_list = tf.readline().strip().split(",")
+
+		#set the letter grid as a dataframe
+		letter_grid = pd.read_csv(tf, header = None)
+
+		print word_list
+		print letter_grid
+
+if __name__ == '__main__':
+	# load in file from command line
+	text_file = sys.argv[-1]
+	main(text_file)
