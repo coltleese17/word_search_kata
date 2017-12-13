ca7e172025172d189c88cdae90988e5e36d67f22
ColtonLeese <coltleese@gmail.com>
2017-12-12 20:34:09 -0500

    switch var y to coord_y and cleaning up typo

--- word_search_kata.py
+++ word_search_kata.py
@@ -7,61 +7,61 @@ def check_letter_match(current_letter,search_letter):
 	else:
 		return False
 
-def check_all_directions(x,y,letter_index,word,letter_grid):
+def check_all_directions(x,coord_y,letter_index,word,letter_grid):
 	default_false_value = False
 
 	#all methods return a list of coordinates for a found word, or False
 
-	south_value = search_south(x,y,letter_index,word,letter_grid)
+	south_value = search_south(x,coord_y,letter_index,word,letter_grid)
 	if (south_value):
 		return south_value
 
-	east_value = search_east(x,y+1,letter_index,word,letter_grid)
+	east_value = search_east(x,coord_y,letter_index,word,letter_grid)
 	if (east_value):
 		return east_value
 
-	west_value = search_west(x,y,letter_index,word,letter_grid)
+	west_value = search_west(x,coord_y,letter_index,word,letter_grid)
 	if (west_value):
 		return west_value	
 
-	north_value = search_north(x,y,letter_index,word,letter_grid)
+	north_value = search_north(x,coord_y,letter_index,word,letter_grid)
 	if (north_value):
 		return north_value
 
-	south_east_value = search_south_east(x,y,letter_index,word,letter_grid)
+	south_east_value = search_south_east(x,coord_y,letter_index,word,letter_grid)
 	if (south_east_value):
 		return south_east_value
 
-	south_west_value = search_south_west(x,y,letter_index,word,letter_grid)
+	south_west_value = search_south_west(x,coord_y,letter_index,word,letter_grid)
 	if (south_west_value):
 		return south_west_value			
 
-	north_west_value = search_north_west(x,y,letter_index,word,letter_grid)
+	north_west_value = search_north_west(x,coord_y,letter_index,word,letter_grid)
 	if (north_west_value):
 		return north_west_value		
 
-	north_east_value = search_north_east(x,y,letter_index,word,letter_grid)
+	north_east_value = search_north_east(x,coord_y,letter_index,word,letter_grid)
 	if (north_east_value):
 		return north_east_value			
 	
 	return default_false_value		
 
-def search_east(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_east(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
-	y += 1
+	coord_y += 1
 	letter_index += 1
 
 	try:
 		
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
 
-			y += 1
+			coord_y += 1
 			letter_index +=1	
 
 		return False
@@ -69,16 +69,16 @@ def search_east(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False	
 
-def search_south(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_south(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
 	x += 1
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
@@ -91,21 +91,21 @@ def search_south(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False	 	
 
-def search_west(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_west(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
-	y -= 1
+	coord_y -= 1
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
 
-			y -= 1
+			coord_y -= 1
 			letter_index +=1	
 
 		return False
@@ -113,16 +113,16 @@ def search_west(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False	 	 
 
-def search_north(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_north(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
 	x -= 1
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
@@ -135,23 +135,23 @@ def search_north(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False	 
 
-def search_north_west(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_north_west(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
 	x -= 1
-	y -= 1 
+	coord_y -= 1 
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
 
 			x -= 1
-			y -= 1
+			coord_y -= 1
 			letter_index +=1	
 
 		return False
@@ -159,23 +159,23 @@ def search_north_west(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False		 			 	 		
 
-def search_south_west(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_south_west(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
 	x += 1
-	y -= 1 
+	coord_y -= 1 
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
 
 			x += 1
-			y -= 1
+			coord_y -= 1
 			letter_index +=1	
 
 		return False
@@ -183,24 +183,24 @@ def search_south_west(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False	
 
-def search_south_east(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_south_east(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
 	x += 1
-	y += 1 
+	coord_y += 1 
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
 
 
 			x += 1
-			y += 1
+			coord_y += 1
 			letter_index +=1	
 	
 		return False
@@ -208,23 +208,23 @@ def search_south_east(x,y, letter_index, word,letter_grid):
 	except (KeyError,IndexError):
 	 		return False	 			 			 	 			 	
 
-def search_north_east(x,y, letter_index, word,letter_grid):
-	coordinates = [(x,y)]
+def search_north_east(x,coord_y, letter_index, word,letter_grid):
+	coordinates = [(x,coord_y)]
 
 	x -= 1
-	y += 1 
+	coord_y += 1 
 	letter_index += 1
 
 	try:
-		while (check_letter_match(letter_grid[x][y], word[letter_index])):
+		while (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-			coordinates.append((x,y))
+			coordinates.append((x,coord_y))
 
 			if (letter_index == (len(word) - 1 )):
 				return coordinates
 
 			x -= 1
-			y += 1
+			coord_y += 1
 			letter_index +=1	
 
 		return False
@@ -249,11 +249,11 @@ def main(text_file):
 
 		for word in word_list:
 			for x in letter_grid:
-				for y in letter_grid:
+				for coord_y in letter_grid:
 
-					if (check_letter_match(letter_grid[x][y], word[letter_index])):
+					if (check_letter_match(letter_grid[x][coord_y], word[letter_index])):
 
-					 	word_coordinates = check_all_directions(x,y,letter_index, word,letter_grid)
+					 	word_coordinates = check_all_directions(x,coord_y,letter_index, word,letter_grid)
 
 					 	if (word_coordinates):
 					 		matches[word] = word_coordinates
--- word_search_kata_specs.py
+++ word_search_kata_specs.py
@@ -11,83 +11,83 @@ letter_grid_3x3 = [['t','h','e'],
 class wordSearchTDD(unittest.TestCase):
 
 	def test_search_east(self):
-		x = 0
-		y = 0 
+		coord_x = 0
+		coord_y = 0 
 		letter_index = 0
 		word = 'the' 
-		coordinates = word_search_kata.search_east(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_east(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(0,0),(0,1),(0,2)] , coordinates)
 
 	def test_search_east_searchword_out_of_bounds(self):
-		x = 0 
-		y = 0 
+		coord_x = 0 
+		coord_y = 0 
 		letter_index = 0
 		word = 'them' 
-		coordinates = word_search_kata.search_east(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_east(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( False , coordinates)
 
 	def test_search_east_searchword_not_found(self):
-		x = 0 
-		y = 0 
+		coord_x = 0 
+		coord_y = 0 
 		letter_index = 0
 		word = 'thx' 
-		coordinates = word_search_kata.search_east(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_east(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( False , coordinates)			
 
 	def test_search_south(self):
-		x = 0
-		y = 0 
+		coord_x = 0
+		coord_y = 0 
 		letter_index = 0
 		word = 'too' 
-		coordinates = word_search_kata.search_south(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_south(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(0,0),(1,0),(2,0)] , coordinates)
 
 	def test_search_west(self):
-		x = 0
-		y = 2 
+		coord_x = 0
+		coord_y = 2 
 		letter_index = 0
 		word = 'eht' 
-		coordinates = word_search_kata.search_west(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_west(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(0,2),(0,1),(0,0)] , coordinates)	
 
 	def test_search_north(self):
-		x = 2
-		y = 0
+		coord_x = 2
+		coord_y = 0
 		letter_index = 0
 		word = 'oot' 
-		coordinates = word_search_kata.search_north(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_north(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(2,0),(1,0),(0,0)] , coordinates)
 
 	def test_search_north_west(self):
-		x = 2
-		y = 2
+		coord_x = 2
+		coord_y = 2
 		letter_index = 0
 		word = 'tot' 
-		coordinates = word_search_kata.search_north_west(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_north_west(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(2,2),(1,1),(0,0)] , coordinates)	
 
 	def test_search_south_west(self):
-		x = 0
-		y = 2
+		coord_x = 0
+		coord_y = 2
 		letter_index = 0
 		word = 'eoo' 
-		coordinates = word_search_kata.search_south_west(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_south_west(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(0,2),(1,1),(2,0)] , coordinates)	
 
 	def test_search_south_east(self):
-		x = 0
-		y = 0
+		coord_x = 0
+		coord_y = 0
 		letter_index = 0
 		word = 'tot' 
-		coordinates = word_search_kata.search_south_east(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_south_east(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(0,0),(1,1),(2,2)] , coordinates)				
 
 	def test_search_north_east(self):
-		x = 2
-		y = 0
+		coord_x = 2
+		coord_y = 0
 		letter_index = 0
 		word = 'ooe' 
-		coordinates = word_search_kata.search_north_east(x,y,letter_index,word,letter_grid_3x3)
+		coordinates = word_search_kata.search_north_east(coord_x,coord_y,letter_index,word,letter_grid_3x3)
 		self.assertEqual( [(2,0),(1,1),(0,2)] , coordinates)		
 
 	def test_main_with_values_not_found_on_grid(self):
@@ -101,7 +101,8 @@ class wordSearchTDD(unittest.TestCase):
 		 					'SULU': [(3, 3), (2, 2), (1, 1), (0, 0)],
 		 					'SCOTTY': [(0, 5), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5)],
 		 					'KIRK': [(4, 7), (3, 7), (2, 7), (1, 7)],
-		 					'SPOCK': [(2, 1), (3, 2), (4, 3), (5, 4), (6, 5)]}, output_of_full_program)	
+		 					'SPOCK': [(2, 1), (3, 2), (4, 3), (5, 4), (6, 5)],
+		 					'BONES': [(0, 6), (0, 7), (0, 8), (0, 9), (0, 10)]}, output_of_full_program)	
 
 
 if __name__ == '__main__':
